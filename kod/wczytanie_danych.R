0a11a71fd0a70ffc4d2bbd2c47ae482e6b05da78
Paulina Przybyłek <paulinaprzybylek@hapus.riviera.ds.pw.edu.pl>
2019-10-07 02:00:43 +0200

    funkcja wczytująca dane z pliku

--- kod/wczytanie_danych.R
+++ kod/wczytanie_danych.R
@@ -26,15 +26,25 @@ tmp <- read_sas(student_pierwszy_plik)
 
 duze_dane <- read.sas7bdat(plik)
 
+# student2_drugi_folder <- file.path(dane.dic, "PUF_SAS_COMBINED_CMB_STU_QQQ")
+# student2_pierwszy_plik <- file.path(student2_drugi_folder, "cy6_ms_cmb_stu_qq2.sas7bdat")
+# tmp1 <- read_sas(student2_pierwszy_plik)
+# 
+# student2_drugi_plik <- file.path(student2_drugi_folder, "cy6_ms_cmb_stu_qqq.sas7bdat") # dużo kolumn, nazwy niektórych są z kweestionariuszy rodziców, o karierze itp.
+# tmp2 <- read_sas(student2_drugi_plik)
 
+# Wczytywanie po kolei wszystkich danych jest żmudne i słabo zrobione - lepiej stowrzyć jedną funkcję
 
+create_data_from_sas_format <- function(path, dictionary, file) {
+  
+  stopifnot(is.character(path), is.character(dictionary), is.character(file))
+  
+  path <- file.path(path, dictionary, file)
+  new_data <- read_sas(path)
+  new_data
+}
 
-
-
-
-
-
-
-
+tmp3 <- create_data_from_sas_format(dane.dic, "PUF_SAS_COMBINED_CMB_STU_QQQ", "cy6_ms_cmb_stu_qq2.sas7bdat")
+#tmp3 == tmp1
 
 
